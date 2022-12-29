4a669f7130f1227137c1296b7f271aa8a2ed8a7e
hugoagno3 <hugoagno3@gmail.com>
2022-12-29 18:50:04 +0100

    change the name to be recognized by mCP

---
+++ R/fdr_mCP.R
@@ -0,0 +1,43 @@
+#' fdr_mCP
+#' 
+#' @description This function performs different matrix from your experimental matrix and evaluetes this "fake matrix" into  mCP workflow by using the selected filter then calculates the FDR based in montecarlo simulations and the real result of protein complexes detected in your experiment.
+#' @param corum_database The corum data base. 
+#' @param experiment_data A matrix that has protein_id in the first column and the detected intensities in the other colums.
+#' @param N_fractions Number of fractions in your experimet
+#' @param filter Pearson Correlation filter for the analysis
+#' @param specie Write: "hsapiens" if you work with homosapiens. 
+#' @param n_simulations number of simulations 10 by default
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+fdr_mCP<- function (corum_database, experiment_data, N_fractions = 35, 
+                    specie = "hsapiens", filter=0.93, n_simulations= 10) 
+{
+  X<- replicate(n_simulations,{
+    Hek_293_fake <- experiment_data
+    Hek_293_fake[,2:ncol(experiment_data)]<- experiment_data[sample(1:nrow(experiment_data),nrow(experiment_data),replace = F),2:ncol(experiment_data)]
+    
+    CL_hek_P1_2_a<- mcp_list(corum_database =  Corum_Humans_Database,
+                             experiment_data = Hek_293_fake, N_fractions = N_fractions, specie = specie)
+    
+    out_Hek_p1_2_a <- cpp_plotter(CL_hek_P1_2_a,output_name = paste0(format(Sys.time(), "%H_%M_%OS3"),"fake.pdf"), format = ".", filter = 0.93, N_fractions = 35,
+                                  display_weights = TRUE, standard_weights =list(list(x = 11, label= "1049KDa"), 
+                                                                                 list(x = 13, label ="720 KDa"),
+                                                                                 list(x = 17, label = "480 KDa"), 
+                                                                                 list(x =22, label ="146 KDa"),
+                                                                                 list(x =29, label ="60 KDa")))
+  }
+  )
+  Analysis_result  <- mcp_list(corum_database =  Corum_Humans_Database,
+                               experiment_data = experiment_data, N_fractions = N_fractions, specie = specie)
+  n_Complexes <- sum(sapply(Analysis_result, function(Comp){
+    return(sum(Comp$corMat >= filter)>nrow(Comp$corMat))
+  }))
+  
+  fdrs <- sapply(X, "length")/n_Complexes
+  print(paste0("FDR (McS)= ", mean(fdrs)))
+  FDRsimulations<- X
+}
