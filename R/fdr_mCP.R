8654b9a93d7a664ad57f878f0c6cce96de252758
hugoagno3 <hugoagno3@gmail.com>
2023-02-08 02:20:53 +0100

    add a part of the function to store and calculate general FDR excel sheet

--- R/fdr_mCP.R
+++ R/fdr_mCP.R
@@ -9,17 +9,18 @@
 #' @param n_simulations number of simulations 10 by default
 #' @param Output_cpp_plotter output list of detected protein complexes function cpp_plotter
 #' @param file_name this is the file name of the output, is a string, It should be writen within "". For example "outputname"
+#' @param save_file could be TRUE or FALSE it generate an csv file with results of Fold Discovery Rate per protein complexes
 #'
 #' @return
 #' @export
 #'
 #' @examples
 
-fdr_mCP<- function (corum_database, experiment_data, N_fractions = 35, 
-                    specie = "hsapiens", filter=0.93, n_simulations= 10, Output_cpp_plotter, file_name= "exp_id") 
+fdr_mCP<-  function (corum_database, experiment_data, N_fractions = 35, 
+                     specie = "hsapiens", filter=0.93, n_simulations= 10, Output_cpp_plotter, file_name= "exp_id", save_file=TRUE) 
 {
-  sink(paste0(file_name, "_FDR.txt"))
-  print("starting Monte Carlo Simulation")
+  #sink(paste0(file_name, "_FDR.txt"))
+  #print("starting Monte Carlo Simulation")
   X<- replicate(n_simulations,{
     experiment_data <- experiment_data
     # Identifie those Protein Ids that are in the data set and in the Corum DB
@@ -34,20 +35,37 @@ fdr_mCP<- function (corum_database, experiment_data, N_fractions = 35,
     # this part we don't need anymore
     #experiment_data[,2:ncol(experiment_data)]<- experiment_data[sample(1:nrow(experiment_data),nrow(experiment_data),replace = F),2:ncol(experiment_data)]
     
-    CL_hek_P1_2_a<- mcp_list(corum_database =  Corum_Database,
+    CL_hek_P1_2_a<- mcp_list(corum_database =  corum_database,
                              experiment_data = experiment_data, N_fractions = N_fractions, specie = specie)
     
-    out_Hek_p1_2_a <- potter(CL_hek_P1_2_a,output_name = paste0(format(Sys.time(), "%H_%M_%OS3"),"fake.pdf"), format = ".", filter = filter, N_fractions = N_fractions,
-                                  display_weights = TRUE, standard_weights =list(list(x = 11, label= "1049KDa"), 
-                                                                                 list(x = 13, label ="720 KDa"),
-                                                                                 list(x = 17, label = "480 KDa"), 
-                                                                                 list(x =22, label ="146 KDa"),
-                                                                                 list(x =29, label ="60 KDa")))
+    out_Hek_p1_2_a <- cpp_plotter(CL_hek_P1_2_a,output_name = paste0(format(Sys.time(), "%H_%M_%OS3"),"fake.pdf"), format = ".", filter = filter, N_fractions = N_fractions,
+                                  display_weights = FALSE)
   }
   )
-  print("Fold discovery rate results")
-  print(paste0("FDR (McS)= ", mean(sapply(X, "length")/length(Output_cpp_plotter))))
-  print(paste0("PPC_Detected= ", length(Output_cpp_plotter)))
-  print(paste0("SD_Fdr= ", sd(sapply(X, "length")/length(Output_cpp_plotter))))
-  sink()
+  
+  complex_names <- unique(corum_database$complex_name[corum_database$protein_id %in% experiment_data$protein_id])
+  FDs <- sapply(complex_names,function(comp_name){
+    return(sum(sapply(X,function(Simulation){
+      return(comp_name %in% names(Simulation))
+    })))
+  })
+  
+  res_DF <- data.frame(complex_names,
+                       Discovered = FDs,
+                       rel.discoveries = FDs/n_simulations,
+                       N_subunits = as.numeric(table(corum_database$complex_name)[complex_names]))
+  
+  if(save_file){
+    write.csv(res_DF[names(Output_cpp_plotter),],file = paste0(file_name,".csv"))
+  }
+  
+  
+  return(res_DF[names(Output_cpp_plotter),])
+  
+  #print("Fold discovery rate results")
+  #print(paste0("FDR (McS)= ", mean(sapply(X, "length")/length(Output_cpp_plotter))))
+  #print(paste0("PPC_Detected= ", length(Output_cpp_plotter)))
+  #print(paste0("SD_Fdr= ", sd(sapply(X, "length")/length(Output_cpp_plotter))))
+  #sink()
+  #return(X)
 }
