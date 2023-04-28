7b27b3e65245fb22aaad8bbf800de5f2de7ed755
hugoagno3 <hugoagno3@gmail.com>
2023-04-28 09:22:36 +0200

    a big modification to select protein complexes by using FDR according to conections between proteins within the protein complexees

--- R/fdr_mCP.R
+++ R/fdr_mCP.R
@@ -17,52 +17,65 @@
 #' @examples
 
 fdr_mCP<-  function (corum_database, experiment_data, N_fractions = 35, 
-                     specie = "hsapiens", filter=0.93, n_simulations= 10, Output_cpp_plotter, file_name= "exp_id", save_file=TRUE) 
+                     specie = "hsapiens", filter = 0.93, n_simulations = 10, 
+                     Output_cpp_plotter, file_name = "exp_id", save_file = TRUE) 
 {
-  X<- replicate(n_simulations,{
+  standar_Experiment<- extract_mcp(Output_cpp_plotter)
+  ######## simulation##########
+  X <- replicate(n_simulations, {
     experiment_data <- experiment_data
-    # Identifie those Protein Ids that are in the data set and in the Corum DB
-    Indeces_in_Corum <- experiment_data$protein_id %in% corum_database$protein_id
-    # choose random Protein Ids from Proteins, that are not in the Corum DB
-    fake_Ids <- sample(experiment_data$protein_id[!Indeces_in_Corum],sum(Indeces_in_Corum),replace = F)
-    # replace choosen Prot ids, from proteins that are not part of a complex with Prot Ids from Prot that are in the Corum DB
-    experiment_data$protein_id[experiment_data$protein_id %in% fake_Ids] <- experiment_data$protein_id[sample(which(Indeces_in_Corum),sum(Indeces_in_Corum),replace = F)]
-    # replace Prot Ids, of proteins that are in the Corum DB with random Protein Ids from Proteins that are not in the Corum DB
+    Indeces_in_Corum <- experiment_data$protein_id %in% 
+      corum_database$protein_id
+    fake_Ids <- sample(experiment_data$protein_id[!Indeces_in_Corum], 
+                       sum(Indeces_in_Corum), replace = F)
+    experiment_data$protein_id[experiment_data$protein_id %in% 
+                                 fake_Ids] <- experiment_data$protein_id[sample(which(Indeces_in_Corum), 
+                                                                                sum(Indeces_in_Corum), replace = F)]
     experiment_data$protein_id[Indeces_in_Corum] <- fake_Ids
-    
-    # this part we don't need anymore
-    #experiment_data[,2:ncol(experiment_data)]<- experiment_data[sample(1:nrow(experiment_data),nrow(experiment_data),replace = F),2:ncol(experiment_data)]
-    
-    CL_hek_P1_2_a<- mcp_list(corum_database =  corum_database,
-                             experiment_data = experiment_data, N_fractions = N_fractions, specie = specie)
-    
-    out_Hek_p1_2_a <- cpp_plotter(complex_list = CL_hek_P1_2_a,output_name = paste0(format(Sys.time(), "%H_%M_%OS3"),"fake.pdf"), format = ".", filter = filter, N_fractions = N_fractions,
-                                  display_weights = FALSE)
-  }
-  )
-  
-  complex_names <- unique(corum_database$complex_name[corum_database$protein_id %in% experiment_data$protein_id])
-  FDs <- sapply(complex_names,function(comp_name){
-    return(sum(sapply(X,function(Simulation){
-      return(comp_name %in% names(Simulation))
-    })))
+    CL_hek_P1_2_a <- mcp_list(corum_database = corum_database, 
+                              experiment_data = experiment_data, N_fractions = N_fractions, 
+                              specie = specie)
+    out_Hek_p1_2_a <- cpp_plotter(complex_list = CL_hek_P1_2_a, 
+                                  output_name = paste0(format(Sys.time(), "%H_%M_%OS3"), 
+                                                       "fake.pdf"), format = ".", filter = filter, 
+                                  N_fractions = N_fractions, display_weights = FALSE)
+    #standar_simulation<- extract_mcp(out_Hek_p1_2_a)
   })
   
-  res_DF <- data.frame(complex_names,
-                       Discovered = FDs,
-                       rel.discoveries = FDs/n_simulations,
-                       N_subunits = as.numeric(table(corum_database$complex_name)[complex_names]))
+  complex_names <-  names(Output_cpp_plotter)
+  FDs <- sapply(complex_names, function(comp_name) {
+    return(sum(sapply(X, function(Simulation) {
+      return(comp_name %in% names(Simulation) && Simulation[[comp_name]][[2]][1,1] >= standar_Experiment[comp_name,"hits"])
+    })))
+  })
+  FDs[is.na(FDs)] <- 0
   
-  if(save_file){
-    write.csv(res_DF[names(Output_cpp_plotter),],file = paste0(file_name,".csv"), row.names = FALSE)
+  res_matrix <- matrix(0,nrow = length(complex_names),ncol = n_simulations,dimnames = list(complex_names,paste0("Simulation_hits_",1:n_simulations)))
+  for(i in 1:n_simulations){
+    res_matrix[names(X[[i]])[names(X[[i]]) %in% rownames(res_matrix)],i] <- sapply(sapply(X[[i]],"[[",2),"[[",1)[names(X[[i]]) %in% rownames(res_matrix)]
+  }
+  ############################################################## 
+  res_DF_1 <- data.frame(complex_names,Hits = standar_Experiment$hits, Discovered = FDs, rel.discoveries =FDs/n_simulations, 
+                         N_subunits = as.numeric(table(corum_database$complex_name)[complex_names]),rbind(res_matrix)) #,rbind(res_matrix) to include the hits in each simulation
+  ################################################################
+  ######################################################
+  #FDR_P11_today_50023 <- FDR_P11_2_801_tests_100
+  VS<-res_DF_1 %>% select(6:(5+n_simulations))
+  VES<-res_DF_1$Hits
+  papa<- as.data.frame(ifelse(VS>=VES,1,0))
+  disco_11<- data.frame(Discovered= rowSums(papa))
+  ##############################################################
+  res_DF <- data.frame(complex_names,Hits = standar_Experiment$hits, Discovered = disco_11$Discovered, rel.discoveries =disco_11$Discovered/n_simulations, 
+                       N_subunits = as.numeric(table(corum_database$complex_name)[complex_names]),rbind(res_matrix)) #,rbind(res_matrix) to include the hits in each simulation
+  ############################################################
+  if (save_file) {
+    write.csv(res_DF, file = paste0(file_name,".csv"), row.names = FALSE)
   }
   sink(paste0(file_name, "_FDR.txt"))
-  #print("starting Monte Carlo Simulation") 
   print("Fold discovery rate results")
   print(paste0("FDR (McS)= ", mean(sapply(X, "length")/length(Output_cpp_plotter))))
   print(paste0("PPC_Detected= ", length(Output_cpp_plotter)))
   print(paste0("SD_Fdr= ", sd(sapply(X, "length")/length(Output_cpp_plotter))))
   sink()
-  #return(X)
-  return(res_DF[names(Output_cpp_plotter),])
+  return(res_DF)
 }
