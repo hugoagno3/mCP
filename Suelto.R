bc7ebf7e8cacfcf43dafdffd1da4d0d156e51a6f
hugoagno3 <hugoagno3@gmail.com>
2023-05-06 22:02:09 +0200

    test

---
+++ Suelto.R
@@ -0,0 +1,25 @@
+use_r()
+getwd()
+devtools::create_package()
+
+library(devtools)
+create_package(".")
+library(devtools)
+load_all()
+library(roxygen2)
+vig
+help (mcp_list)
+
+devtools::document()
+help(fdr_mCP)
+usethis::use_vignette()
+help("use_vignette")
+FDR_mCP
+
+
+mcp_list
+vignette("roxygen2")
+vignette("rd")
+help )
+
+help(mCP)
