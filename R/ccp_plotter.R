36841a2bc1e5b36741d6c05386566eac093da8e1
hugoagno3 <hugoagno3@gmail.com>
2023-05-22 02:23:19 +0200

    CHANGE BY IS.NA

--- R/ccp_plotter.R
+++ R/ccp_plotter.R
@@ -82,7 +82,7 @@ cpp_plotter <- function (relative= FALSE, heat_map= FALSE, heatmap_seaborn= FALS
     data <- complex_list[[i]]$data
     corMat <- complex_list[[i]]$corMat
     ########### interactomics
-    if(sum(dim(corMat))==0 | nrow(data) < N_fractions | all(data["Intensity"][complete.cases(data)] == 0)){
+    if(sum(dim(corMat))==0 | nrow(data) < N_fractions | all(data["Intensity"][!is.na(data$Intensity)] == 0)){
       next
     }
     pectores<- corMat
@@ -142,7 +142,7 @@ cpp_plotter <- function (relative= FALSE, heat_map= FALSE, heatmap_seaborn= FALS
     for (i in seq_along(complex_list)) {
       data <- complex_list[[i]]$data
       corMat <- complex_list[[i]]$corMat
-      if(sum(dim(corMat))==0 | nrow(data) < N_fractions | all(data["Intensity"][complete.cases(data)] == 0)){
+      if(sum(dim(corMat))==0 | nrow(data) < N_fractions | all(data["Intensity"][!is.na(data$Intensity)] == 0)){
         next
       } 
       
